4be4275be511d9f1c10c796e3f476d89c63186a2
viny <viny0698@gmail.com>
2023-04-28 11:00:11 +0530

    fix eslint export bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { createInitialBoard, createNewState, checkGameOver } from "./game";
+import { createInitialBoard, createNewState, checkGameOver } from "./game.js";
 import Header from "./components/Header";
 import Board from "./components/Board";
 
